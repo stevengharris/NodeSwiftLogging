35d61ee6eafbb2c600dd8be3b57ad3f99c4f82a4
Steven G. Harris <steven.g.harris@gmail.com>
2025-04-02 09:36:34 -0700

    Update gitignore and files definition

--- Example/index.mjs
+++ Example/index.mjs
@@ -1,4 +1,8 @@
-const { NodeConsole, testNodeConsole, testLogger } = require("./.build/Module.node");
+//const { NodeConsole, testNodeConsole, testLogger } = require('./build/Module.node');
+//const { NodeConsole, testNodeConsole, testLogger } = require('nslogging/.build/Module.node');
+import { NodeConsole, testNodeConsole, testLogger } from './.build/Module.node'
+
+console.log("NSLoggingExample...");
 
 // Register the callback from Swift, including swift-log Logger info and higher messages
 const nodeConsole = new NodeConsole();
