b4d82e503ad95b512d502fb8659e07bfd07acd5d
Steven G. Harris <steven.g.harris@gmail.com>
2025-04-03 16:00:21 -0700

    Update example Package.swift

--- Example/Package.swift
+++ Example/Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/kabiroberai/node-swift.git", from: "1.3.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
-        .package(path: ".."),   // <- For development convenience here, but perhaps better to use...
+        .package(name: "NodeSwiftLogging", path: ".."),   // <- For development convenience here, but perhaps better to use...
         // .package(url: "https://github.com/stevengharris/NodeSwiftLogging.git", branch: "main"),
     ],
     targets: [
